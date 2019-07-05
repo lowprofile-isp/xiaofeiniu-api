1f13de03ca98942dc09e097dee4d0ca49a47059e
Lcc <1069625135@qq.com>
2019-07-05 18:10:37 +0800

    项目部署

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var app = express();
 // 使用中间件
 // 解决跨域
 app.use(cors({
-	origin:'*'
+	origin:'http://xfnadmins.applinzi.com'
 }))
 // app.use(bodyParser.urlencoded({}))	//把application/x-www-form-urlencoded格式的请求主体数据
 app.use(bodyParser.json());//把application/JSON格式的请求主体数据解析出来放入req.body属性
